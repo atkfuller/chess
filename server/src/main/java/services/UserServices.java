e2de7446699d36b97a188339cc652e0ea7a5b069
ashleyfuller <atkfuller@gmail.com>
2025-05-20 13:58:41 -0600

    userServices updated

--- server/src/main/java/services/UserServices.java
+++ server/src/main/java/services/UserServices.java
@@ -2,6 +2,7 @@ package services;
 
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
 import dataaccess.UserDAO;
 
 import java.AuthData;
@@ -9,20 +10,25 @@ import java.UserData;
 import java.util.UUID;
 
 public class UserServices {
+    private final UserDAO userAccess;
+    private final AuthDAO authAccess;
+    private final GameDAO gameAccess;
+    public UserServices(){
+        userAccess= new UserDAO();
+        authAccess= new AuthDAO();
+        gameAccess= new GameDAO();
+    }
     public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
-        UserDAO dao = null;
-        UserData user= dao.getUser(registerRequest.username());
+       UserData user=userAccess.getUser(registerRequest.username());
         if(user!=null){
-            throw DataAccessException;//should be a alreadytaken exception
+            throw new DataAccessException("already taken username");
         }
         else{
             user= new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
-            dao.createUser(user);
+            userAccess.createUser(user);
             AuthData auth= new AuthData(generateToken(), registerRequest.username());
-            AuthDAO aDao=null;
-            aDao.createAuth(auth);
-            RegisterResult result= new RegisterResult(registerRequest.username(), auth.authToken());
-            return result;
+            authAccess.createAuth(auth);
+            return new RegisterResult(registerRequest.username(), auth.authToken());
         }
     }
 
